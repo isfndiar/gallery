917e69ace931f43b687c0a5c08e72a8af835c047
isfandiar adi <141855809+isfndiar@users.noreply.github.com>
2023-08-19 09:19:17 +0700

    menyelesaikan tugas 1 membuat gallery

--- script.js
+++ script.js
@@ -3,9 +3,41 @@ document.querySelector("#hamburger-button").onclick = () => {
   navigation.classList.toggle("active");
 };
 
+// close icon
+const cb = document.querySelector("#close-bar");
+
 const hb = document.querySelector("#hamburger-button");
 document.addEventListener("click", function (e) {
   if (!hb.contains(e.target) && !navigation.contains(e.target)) {
     navigation.classList.remove("active");
   }
 });
+
+document.addEventListener("click", function (e) {
+  if (cb.contains(e.target)) {
+    navigation.classList.remove("active");
+  }
+});
+
+// ketika img di klik
+const modalModalan = document.querySelector("#modal-modalan");
+const imageButtons = document.querySelectorAll(".img-button");
+
+imageButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    modalModalan.style.display = "flex";
+    e.prevenDefault();
+  };
+});
+
+// klick
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  modalModalan.style.display = "none";
+  e.prevenDefault();
+};
+
+window.onclick = (e) => {
+  if (e.target === modalModalan) {
+    modalModalan.style.display = "none";
+  }
+};
