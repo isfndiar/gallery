deadd70d16019c9368c3d0ca8da39e15ab50383b
isfandiar adi <141855809+isfndiar@users.noreply.github.com>
2023-10-09 08:58:18 +0700

    feat & fix : display grid, hover image, click image, hamburger button if click, container transition. Bug dikit Gk ngaruh

--- script.js
+++ script.js
@@ -1,43 +1,59 @@
+function openNav() {
+  // container.classList.toggle("active");
+  container.style.marginRight = "250px";
+}
+
+function closeNav() {
+  container.style.marginRight = "0";
+}
+
+const hButton = document.querySelector("#hamburger-button");
 const navigation = document.querySelector(".nav");
-document.querySelector("#hamburger-button").onclick = () => {
+const closeBar = document.querySelector("#close-bar");
+const modal = document.querySelector("#modal");
+const images = document.querySelectorAll(".img-button");
+const imgModal = document.querySelector("#modal-image");
+const container = document.querySelector("#container");
+
+hButton.onclick = async () => {
   navigation.classList.toggle("active");
+  if (navigation.classList.contains("active")) {
+    await openNav();
+  } else {
+    await closeNav();
+  }
 };
+
+for (let image of images) {
+  image.onclick = () => {
+    images.forEach((img) => {
+      img.style.opacity = "1";
+    });
+    console.dir(image);
+    modal.style.display = "flex";
+    image.style.opacity = "0.5";
+  };
+}
+
+container.addEventListener("click", function (e) {
+  imgModal.src = e.target.src;
+});
 
 // close icon
-const cb = document.querySelector("#close-bar");
-
-const hb = document.querySelector("#hamburger-button");
 document.addEventListener("click", function (e) {
-  if (!hb.contains(e.target) && !navigation.contains(e.target)) {
+  if (!hButton.contains(e.target) && !navigation.contains(e.target)) {
     navigation.classList.remove("active");
+    closeNav();
+  }
+
+  if (closeBar.contains(e.target)) {
+    navigation.classList.remove("active");
+    closeNav();
   }
 });
-
-document.addEventListener("click", function (e) {
-  if (cb.contains(e.target)) {
-    navigation.classList.remove("active");
-  }
-});
-
-// ketika img di klik
-const modalModalan = document.querySelector("#modal-modalan");
-const imageButtons = document.querySelectorAll(".img-button");
-
-imageButtons.forEach((btn) => {
-  btn.onclick = (e) => {
-    modalModalan.style.display = "flex";
-    e.prevenDefault();
-  };
-});
-
-// klick
-document.querySelector(".modal .close-icon").onclick = (e) => {
-  modalModalan.style.display = "none";
-  e.prevenDefault();
-};
 
 window.onclick = (e) => {
-  if (e.target === modalModalan) {
-    modalModalan.style.display = "none";
+  if (e.target === modal) {
+    modal.style.display = "none";
   }
 };
